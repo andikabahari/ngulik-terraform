7e8c23021f59b49ceeeb8d37f0717ee224232dcb
Andika Bahari <kzlfxx@gmail.com>
2022-06-18 15:49:05 +0700

    data source & subnet

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ resource "google_compute_subnetwork" "dev_subnet_01" {
   ip_cidr_range = "10.100.0.0/16"
   network       = google_compute_network.dev_net.id
 }
+
+data "google_compute_network" "default_network" {
+  name = "default"
+}
+
+resource "google_compute_subnetwork" "dev_subnet_02" {
+  name          = "dev-subnet-02"
+  ip_cidr_range = "10.110.0.0/16"
+  network       = data.google_compute_network.default_network.id
+}
