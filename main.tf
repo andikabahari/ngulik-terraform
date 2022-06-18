43f331e6aa4cef4d472ac0919322051af4a86c86
Andika Bahari <kzlfxx@gmail.com>
2022-06-18 15:15:02 +0700

    network

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ provider "google" {
   project = "centering-crow-351309"
   region  = "asia-southeast2"
 }
+
+resource "google_compute_network" "dev_net" {
+  name                    = "dev-net"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "dev_subnet_01" {
+  name          = "dev-subnet-01"
+  ip_cidr_range = "10.100.0.0/16"
+  network       = google_compute_network.dev_net.id
+}
